0032116d1cbb34d3cd22237934028cdcc5ac2064
YU KENT <tianyu.tina@gmail.com>
2023-07-24 12:18:09 +0200

    SubHeaderComponent  ameliorer

--- src/compenent/containers/SubHeaderComponent.js
+++ src/compenent/containers/SubHeaderComponent.js
@@ -1,22 +1,12 @@
-import { useState } from "react";
+
 
 function SubHeaderComponent ({onValueChange,NumperPageChange}){
-    const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
-    const handleSearch = (value) => {
-         onValueChange(value);
-         setResetPaginationToggle(!resetPaginationToggle);
-       };
-     
-    const handleCategoryChange = (value) => {
-         console.log('childComponent,NumperPageChange',value)
-         NumperPageChange(value);
-         setResetPaginationToggle(!resetPaginationToggle);
-       };
+
     return(
          <div className="table_subHeader">
              <div className="categories">
                  <span>Show</span>
-                 <select onChange={(e) => handleCategoryChange(e.target.value)}>
+                 <select onChange={(e) => NumperPageChange(e.target.value)}>
                      <option value="10">10</option>
                      <option value="25">25</option>
                      <option value="50">50</option>
@@ -27,7 +17,7 @@ function SubHeaderComponent ({onValueChange,NumperPageChange}){
              <div className='table_search'>
                 <label>Search:</label>
                 <input type="text" placeholder=''
-                    onChange={(e) => handleSearch(e.target.value)}
+                    onChange={(e) => onValueChange(e.target.value)}
                 />
              </div>
          
